94aac2f4a9edb0ce45a9b9213e03397bbdd64bdb
forumulator <forumulator@gmail.com>
2017-08-14 16:59:12 +0530

    Asking user for pages to crawl

--- Crawler/testCrawler.py
+++ Crawler/testCrawler.py
@@ -3,12 +3,13 @@ import logger
 from spider_controller import spider_controller
 
 url = 'https://en.wikipedia.org/wiki/Rick_and_Morty'
+pages_to_crawl = int(input("Enter pages to crawl: "))
 
 proxyDict = {'http' : 'http://<proxy_username>:<proxy_passwd>@202.141.80.22:3128',
             'https': 'https://<proxy_username>:<proxy_passwd>@202.141.80.22:3128'}
 
 logger.initialize()
-spider = spider_controller(5, 500, url)
+spider = spider_controller(5, pages_to_crawl, url)
 spider.run()
 
 print("Done!")
